5fa9b8e76fe1b917436caabcddc7353b51c0f0fb
kushagra05121987 <kushagra.mishra05121987@gmail.com>
2017-11-17 02:29:44 +0530

    deleteing everything

--- include/left-side.php
+++
@@ -1,7 +0,0 @@
-<div id="left-sidebar">
-    <?php
-        echo strftime("%H:%M:%S", time());
-    ?>
-    <br />
-    Hi this is left sidebar
-</div>
--- learning/strict_mode_include.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 18/10/17
- * Time: 11:34 AM
- */
-declare(strict_types=1);
