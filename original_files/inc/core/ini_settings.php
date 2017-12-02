0c184a4668980fe5266ffda0b411b19f69a70033
kushagra05121987 <kushagra.mishra05121987@gmail.com>
2017-12-03 00:09:51 +0530

    2

--- original_files/inc/core/ini_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 23/10/17
- * Time: 10:55 PM
- */
-ini_set("display_errors", '1');
-error_reporting(E_ALL);
--- original_files/include/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <?php
-        echo strftime("%H:%M:%S", time());
-    ?>
-    <br />
-    Hi this is a footer
-</footer>
