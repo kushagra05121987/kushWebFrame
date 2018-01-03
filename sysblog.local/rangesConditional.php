1791c421aca9248ab5f9e4ac7f2525d730bc062f
Kushagra Mishra <kushagra.mishra05121987@gmail.com>
2018-01-03 10:40:55 +0530

    2

--- sysblog.local/rangesConditional.php
+++ sysblog.local/rangesConditional.php
@@ -1,3 +1,65 @@
 <?php
-	
+	header("Access-Control-Allow-Origin: http://localhost:9000");
+	header("Access-Control-Allow-Methods: GET, HEAD");
+	header("Access-Control-Expose-Headers: Custom, Accept-Ranges, Content-Disposition, Content-Length, Etag");
+	header("Custom: custom-x");
+    ob_end_clean();
+    ini_set("output_buffering", "off");
+    // ignore_user_abort(true);
+    $requestHeaders = array();
+	if(! function_exists('getallheaders')) {
+		function getallheaders(&$requestHeaders) {
+			foreach ($_SERVER as $key => $value) { 
+				if(strpos($key, "HTTP_") === 0) {
+					$requestHeaders[$key] = $value;
+				}
+			}
+		}
+		getallheaders($requestHeaders);
+	}
+    // print_r($requestHeaders);
+    if(array_key_exists('HTTP_IF_NONE_MATCH', $requestHeaders) || array_key_exists('HTTP_IF_MATCH', $requestHeaders) || array_key_exists('HTTP_IF_MODIFIED_SINCE', $requestHeaders) || array_key_exists('HTTP_IF_UNMODIFIED_SINCE', $requestHeaders) || array_key_exists('HTTP_IF_RANGE', $requestHeaders)) {
+    } else {
+        
+        $handle = fopen("annual_report_2009.pdf", "r+");
+        $read1 = fread($handle, 10500000);
+        fseek($handle, 10500000);
+        $read2 = fread($handle, (10762150 - 10500000));
+        fclose($handle);
+        $fileContents = file_get_contents('annual_report_2009.pdf');
+        $etag = md5($fileContents);
+        if($_SERVER["REQUEST_METHOD"] == "HEAD") {
+ 			header("Etag: $etag");
+			header("Content-Length: ". strlen($fileContents));
+        } else {
+        	header("Etag: $etag");
+			header("Content-Length: 2000" );
+        	header("Accept-Ranges: bytes");
+        	// header("Content-Type: application/pdf");
+        	echo gettype(filesize('annual_report_2009.pdf'));
+        	echo $read1;
+        	flush();
+	        sleep(1);
+	        echo $read1;
+	        flush();
+	        sleep(1);
+        }
+        // header("HTTP/1.1: 200 OK");
+        // header("Content-Disposition: attachment; filename=annual_report_2009.pdf");
+        // header("Content-Length: ". strlen($fileContents));
+        
+        // $payload = ["name" => "Kushagra Mishra", "age" => 30, "gender" => "male", "location" => "india", "wife" => "Ekta Mishra"];
+        // $etag = md5(json_encode($payload));
+        
+        
+        
+        // $jsonClass = ["name" => "Kushagra Mishra", "age" => 30, "gender" => "male", "location" => "india", "wife" => "Ekta Mishra"];
+        // foreach($jsonClass as $key => $value) {
+        //     echo $value;
+        //     // ob_flush();
+        //     flush();
+        //     sleep(1);
+        // }
+        // echo "OK";
+    }
 ?>
--- ui/src/conditional.js
+++ ui/src/conditional.js
@@ -1,7 +1,64 @@
-fetch("http://sysblog.local:8080/conditional.php").then(function(response) {
-    console.log(response);
-    // console.log(response.json());
-    return response.json();
-}).then(function(json) {
-    $(".include_json").text(JSON.stringify(json));
+// $.ajaxSetup({
+//     // dataType: 'json'
+    
+// });
+
+var responseXHR;
+var responseXHRLength;
+var initialContentLength;
+var Etag;
+// var get = $.get("http://localhost/learning/rangesConditional.php");
+// var ajax = $.ajax({
+//     url: "http://sysblog.local:8080/rangesConditional.php",
+//     type: "HEAD",
+//     success: function(response, texStatus, jqXHR) {
+//         console.log("success", jqXHR.getAllResponseHeaders())
+//         initialContentLength = jqXHR.getResponseHeader('Content-Length')
+//         Etag = jqXHR.getResponseHeader('Etag')
+        
+//     }
+// });
+
+
+var ajax = $.ajax({
+    url: "http://sysblog.local:8080/rangesConditional.php",
+    type: "GET",
+    beforeSend: function() {
+        setTimeout(function() {
+            console.log("TIMEOUT");
+            ajax.abort();
+        }, 3000);
+    },
+    xhr: function() {
+        var xhr = $.ajaxSettings.xhr();
+        xhr.onprogress = function(e) {
+            console.log(e);
+            responseXHR = xhr.responseText;
+            responseXHRLength = responseXHR.length
+        }
+        xhr.onabort = function(e) {
+            console.log("Error", e);
+        }
+        return xhr;
+    },
+    success: function(response, texStatus, jqXHR) {
+        console.log("success", jqXHR.getAllResponseHeaders())
+        initialContentLength = jqXHR.getResponseHeader('Content-Length')
+        Etag = jqXHR.getResponseHeader('Etag')
+    }
 });
+
+// var req = new XMLHttpRequest();
+// req.open("GET", "http://localhost/learning/rangesConditional.php", true);
+// req.onreadystatechange = function(e) {
+//     console.log(req.response, req.responseText, req.readyState);
+//     if(req.readyState === 4) {
+//         console.log(req.response, req.responseText);
+//     }
+// }
+// req.onprogress = function(e) {
+//     console.log(e);
+// }
+// req.send();
+// get.abort();
+console.log(ajax)
